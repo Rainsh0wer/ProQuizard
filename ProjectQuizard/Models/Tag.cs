2c908923c5d9e9fe9fd3cb0c7f6928b5de0e94ca
MinhNhatA3 <88643204+Rainsh0wer@users.noreply.github.com>
2025-07-25 21:25:17 +0700

    Base

---
+++ ProjectQuizard/Models/Tag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectQuizard.Models;
+
+public partial class Tag
+{
+    public int TagId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Question> Questions { get; set; } = new List<Question>();
+}
